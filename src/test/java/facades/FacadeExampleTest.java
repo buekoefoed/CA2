bfea903584c524cfd9a0438ea6474e243bc325e9
Matias <buekoefoed@gmail.com>
2019-10-07 10:09:18 +0200

    reenabled tests

--- src/test/java/facades/FacadeExampleTest.java
+++ src/test/java/facades/FacadeExampleTest.java
@@ -14,7 +14,7 @@ import utils.EMF_Creator.DbSelector;
 import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
-@Disabled
+//@Disabled
 public class FacadeExampleTest {
 
     private static EntityManagerFactory emf;
