f58d98cd08fa25f60b8fe0b957d43e196926812c
Matias <buekoefoed@gmail.com>
2019-10-09 20:12:23 +0200

    Merged with Karl

--- target/generated-sources/annotations/entities/CityInfoEntity_.java
+++ target/generated-sources/annotations/entities/CityInfoEntity_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-09T13:34:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-09T20:11:43")
 @StaticMetamodel(CityInfoEntity.class)
 public class CityInfoEntity_ { 
 
--- target/generated-sources/annotations/entities/HobbyEntity_.java
+++ target/generated-sources/annotations/entities/HobbyEntity_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-09T12:59:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-09T20:11:43")
 @StaticMetamodel(HobbyEntity.class)
 public class HobbyEntity_ { 
 
