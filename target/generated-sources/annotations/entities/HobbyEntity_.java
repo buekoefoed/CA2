2d2223f4af8c6cd943155bf5f67b48d91820c5de
Matias <buekoefoed@gmail.com>
2019-10-09 10:58:02 +0200

    merged with KarlBranch

--- target/generated-sources/annotations/entities/HobbyEntity_.java
+++ target/generated-sources/annotations/entities/HobbyEntity_.java
@@ -1,18 +1,21 @@
 package entities;
 
 import entities.PersonEntity;
+import java.util.Date;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-09T10:44:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-09T10:57:08")
 @StaticMetamodel(HobbyEntity.class)
 public class HobbyEntity_ { 
 
     public static volatile ListAttribute<HobbyEntity, PersonEntity> persons;
+    public static volatile SingularAttribute<HobbyEntity, Date> created;
     public static volatile SingularAttribute<HobbyEntity, String> name;
     public static volatile SingularAttribute<HobbyEntity, String> description;
     public static volatile SingularAttribute<HobbyEntity, Integer> id;
+    public static volatile SingularAttribute<HobbyEntity, Date> lastEdited;
 
 }
